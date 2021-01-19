3d6d2ad68effa4b7b7839513c706e026644da550
Ahmed-aed <aeid767@gmail.com>
2021-01-19 17:19:15 +0200

    soluation Exercise-3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import Counter from './Exercise-1'
-import MouseMove from './Exercise-2'
-;
+import "./App.css";
+import Counter from "./Exercise-1";
+import MouseMove from "./Exercise-2";
+import Giphy from "./Exercise-3";
 
 function App() {
   return (
     <div className="App">
-     <Counter/>
-     <MouseMove/>
+      <Counter />
+      <MouseMove />
+      <Giphy />
     </div>
   );
 }
